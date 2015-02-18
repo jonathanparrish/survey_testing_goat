1a977820da593d2d5598bf44edcf7c5d6d320f28
Jonathan Parrish <yoda.peh@gmail.com>
2015-02-18 18:04:07 -0500

    Add gem, add email test/fixture

--- test/controllers/authors_controller_test.rb
+++ test/controllers/authors_controller_test.rb
@@ -2,4 +2,9 @@ require 'test_helper'
 
 class AuthorsControllerTest < ActionController::TestCase
 
+  setup do
+    @author = authors(:genesis)
+  end
+
+
 end
--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -4,4 +4,8 @@ class AuthorTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+
+  test "check that email" do
+  assert_equal "zane@cain.org", authors(:genesis).email
+  end
 end
